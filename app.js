11079cc02c481ceefb9630476544e73f33bc14c1
codecamp17 <guptamuskan495@gmail.com>
2020-08-06 11:28:18 +0530

    Api

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 angular.module('CovidSearch', ['ngResource'])
-      .factory('Covid', function($resource) {
-        return $resource( 'https://coronavirus-19-api.herokuapp.com/countries/{countryname}?countryname=:query',{query: '@query'}, {
-          search: {
-            method: 'JSONP',
-            params: {
-                   callback: 'JSON_CALLBACK'
-            }
-          }
+  .config(function ($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      // Allow same origin resource loads.
+      'self',
+      // Allow loading from our assets domain. **.
+      'https://coronavirus-19-api.herokuapp.com/**'
+    ])
+  })
+  .factory('Covid', function ($http) {
+    return {
+      search: function () {
+        return $http({
+          method: 'GET',
+          url: 'https://coronavirus-19-api.herokuapp.com/countries'
         });
-      })
-      .controller('CovidCtrl', function($scope, Covid) {
-        $scope.query = '';
-        $scope.$watchCollection('query', function() {
-          if ($scope.query.length > 0) {
-            Covid.search({ query: $scope.query}, function(data) {
-              $scope.results = data.results;
-            });
-          } else {
-            $scope.results = [];
-          }
-        });
-      });
-
+      }
+    }
+  })
+  .controller('CovidCtrl', function ($scope, Covid) {
+    $scope.query = 'a'
+    $scope.onKeyupSearch = function () {
+      Covid.search({ query: $scope.query }).then(function (data) {
+        console.log(data)
+        $scope.results = data.data;
+      }).catch(function (eror) {});
+    }
+  });
